3ca5894318bfb9b6c3b1cb0406555a9047504dfd
koedbymonkey <tsnavely4@gmail.com>
2013-07-02 17:51:48 -0700

    I: add :admin role to Story.attr_accessible
    [closes #2]

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,8 @@
 class Story < ActiveRecord::Base
 
-  attr_accessible :body, :title
-  
+  attr_accessible :body, :title, as: [:admin, :default]
+  attr_accessible :author_id,    as: [:admin]
+
   belongs_to :author, class_name: 'User', :inverse_of => :stories
 
   validates :author, presence: true
