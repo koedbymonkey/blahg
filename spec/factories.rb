1ca82d1db2fde1498f1caa7517a23efe75a60c19
koedbymonkey <tsnavely4@gmail.com>
2013-07-18 17:31:43 -0700

    add story factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,19 @@
 FactoryGirl.define do
 
+  factory :story do
+    author
+    body   'The best post evah!'
+
+    sequence(:title) { |n| "Story #{ n }" }
+  end
+
   factory :unconfirmed_user, class: User do
     sequence(:email) { |n| "user#{ n }@testing.com" }
 
     password              'password'
     password_confirmation 'password'
 
-    factory :user do
+    factory :user, aliases: [:author] do
       after(:create) { |user, proxy| user.confirm! }
 
       factory :admin do
