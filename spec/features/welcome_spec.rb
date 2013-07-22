6d7106aed3b0f1b9578a70d56fd556e5561330fe
koedbymonkey <tsnavely4@gmail.com>
2013-07-21 17:33:26 -0700

    Added welcome spec

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'Welcome' do
+
+  subject { page }
+
+  let(:path) { root_path }
+
+  before do
+    visit path
+  end
+
+  describe 'permissions' do
+
+    it { should allow_access.to(:guest) }
+
+  end
+
+  describe 'when allowed' do
+
+    include ActionController::RecordIdentifier 
+
+    before do
+      FactoryGirl.create_list :story, 3
+      visit path
+    end
+
+    it 'includes all stories' do
+      story.map { |s| should have_css("##{ dom_id s }") }
+    end 
+
+  end
+
+end
